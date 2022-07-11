5e2f3b78bb6ad5a8346be4167fb274092d7832ef
Kate Roll <kater@alleninstitute.org>
2022-07-11 11:01:13 -0700

    added get_mouseseeks_qc_results from database

--- mindscope_qc/data_access/from_mouseseeks.py
+++ mindscope_qc/data_access/from_mouseseeks.py
@@ -8,7 +8,7 @@ def add_behavior_record(behavior_session_uuid=None, pkl_path=None, overwrite=Fal
         - licks
         - time
         - rewards
-        - visual_stimuli
+        - visual_stimulis
         - omitted_stimuli
         - metadata
         - log
@@ -28,3 +28,18 @@ def add_behavior_record(behavior_session_uuid=None, pkl_path=None, overwrite=Fal
         data_to_change_detection_core = foraging1_translator
     else:
         raise NameError('data_type must be either `foraging1` or `foraging2`')
+
+
+
+
+def get_mouseseeks_qc_results(session_id=None, id_type='behavior_session_uuid'):
+    '''get qc results from mouseseeks'''
+    session_id, id_type = _check_name_schema('mouseseeks', session_id, id_type)
+    if id_type == 'foraging_id':
+        session_id = convert_id({'foraging_id': session_id}, 'behavior_session_id')
+
+    mouseseeks = Database('mouseseeks')
+    res = list(mouseseeks.qc.metrics.find({'behavior_session_id': session_id}))
+    mouseseeks.close()
+    if len(res) > 0:
+        return res[0]
