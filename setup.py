e6d320f6926a81d83b1e0a925537fdf78badd670
Sean McCulloch <sean.mcculloch@alleninstitute.org>
2022-09-01 14:00:42 -0700

    modify VBA source link

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='mindscope_qc',
         'flake8',
         'pytest',
         'allensdk',
-        'visual_behavior @ ssh+git//git@github.com:AllenInstitute/visual_behavior_analysis.git'
+        'visual_behavior @ git+https://github.com/AllenInstitute/visual_behavior_analysis.git'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
