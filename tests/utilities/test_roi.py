bef59a5b825dbdaa64d631982848c36c725e1ecf
Sean McCulloch <sean.mcculloch@alleninstitute.org>
2022-09-02 12:06:56 -0700

    add second test for load_legacy_rois

--- tests/utilities/test_roi.py
+++ tests/utilities/test_roi.py
@@ -8,3 +8,10 @@ def test_load_legacy_rois():
     expected_result = expected_result[expected_result.loc[:," eXcluded"] == 0]
     result = roi.load_legacy_rois(1198067556)
     pd.testing.assert_frame_equal(result,expected_result)
+
+@pytest.mark.onprem
+def test_load_legacy_rois__exclude_rois_false():
+    expected_data_filepath = '//allen/programs/mindscope/production/learning/prod0/specimen_1187454009/ophys_session_1197920532/ophys_experiment_1198067556/processed/ophys_cell_segmentation_run_1198081388/objectlist.txt'
+    expected_result = pd.read_csv(expected_data_filepath, sep=',',header=0)
+    result = roi.load_legacy_rois(1198067556, False)
+    pd.testing.assert_frame_equal(result,expected_result)
