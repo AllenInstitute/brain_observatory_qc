2072397068195152742f58581d10493b89b058b9
Kate R <kater@alleninstitute.org>
2022-09-04 17:19:16 -0700

    removed get_synch_path
    out of date function for getting the path to the sync file

--- mindscope_qc/data_access/from_network.py
+++ mindscope_qc/data_access/from_network.py
@@ -25,21 +25,6 @@ def load_image(image_filepath: str) -> np.ndarray:
 #             STORAGE DIRECTORIES
 ######################################################
 
-def get_sync_path(lims_data):
-    ophys_session_dir = get_ophys_session_dir(lims_data)
-
-    sync_file = [file for file in os.listdir(ophys_session_dir) if 'sync' in file]
-    if len(sync_file) > 0:
-        sync_file = sync_file[0]
-    else:
-        json_path = [file for file in os.listdir(ophys_session_dir) if '_platform.json' in file][0]
-        with open(os.path.join(ophys_session_dir, json_path)) as pointer_json:
-            json_data = json.load(pointer_json)
-            sync_file = json_data['sync_file']
-    sync_path = os.path.join(ophys_session_dir, sync_file)
-    return sync_path
-
-
 def get_experiment_storage_directory(ophys_experiment_id: int) -> str:
     """gets the experiment level storage directory filepath for a 
     specific ophys experiment
