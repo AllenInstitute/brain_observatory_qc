7a65022f3d00059d1c36d7446e5417089061a979
Kate R <kater@alleninstitute.org>
2022-09-06 12:50:43 -0700

    general update
    removed unnecessary imports
    standardized get filepath functions
    standardized load image functions

--- mindscope_qc/data_access/from_network.py
+++ mindscope_qc/data_access/from_network.py
@@ -1,12 +1,11 @@
 import os
+from os.path import exists as file_exists
 import sys
 import numpy as np
-from tkinter import Image
-import pandas as pd
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-import mindscope_qc.data_access.utilities as utils
 import mindscope_qc.data_access.from_lims as lims
 
 ######################################################
@@ -14,7 +13,11 @@ import mindscope_qc.data_access.from_lims as lims
 ######################################################
 def get_filepath(storage_directory: str, filename: str) -> str:
     filepath = os.path.join(storage_directory, filename)
-    return filepath
+    if file_exists(filepath) is False:
+        no_file_warning = "Warning: {} does not exist".format(filepath)
+        print(no_file_warning)
+    else:
+        return filepath
 
 def load_image(image_filepath: str) -> np.ndarray:
     image = mpimg.imread(image_filepath)
@@ -24,6 +27,9 @@ def load_image(image_filepath: str) -> np.ndarray:
 ######################################################
 #             STORAGE DIRECTORIES
 ######################################################
+def get_specimen_storage_directory(specimen_id: int) -> str:
+    pass
+
 
 def get_experiment_storage_directory(ophys_experiment_id: int) -> str:
     """gets the experiment level storage directory filepath for a 
@@ -153,7 +159,6 @@ def get_current_cell_segmentation_run_directory(ophys_experiment_id: int) -> str
 #             SPECIMEN LEVEL FILES
 ######################################################
 
-
 def get_post_surgical_photodoc_PNG_filepath(specimen_id: int) -> str:
     storage_directory = get_specimen_storage_directory(specimen_id)
     filename = '1_{}-0000.png'.format(specimen_id)
@@ -220,12 +225,115 @@ def get_cortical_zstack_TIFF_filepath(ophys_session_id: int) -> str:
     filepath = get_filepath(storage_directory, filename)
     return filepath
 
+######################################################
+#             EYE TRACKING FILES
+######################################################
+def get_ellipse_H5_filepath(ophys_session_id: int) -> str:
+    storage_directory = get_eye_tracking_storage_directory(ophys_session_id)
+    filename = '{}_ellipse.h5'.format(ophys_session_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
 
 
 ######################################################
 #             EXPERIMENT LEVEL FILES
 ######################################################
+def get_experiment_H5_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_storage_directory(ophys_experiment_id)
+    filename = '{}.h5'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
 
+def get_surface_TIF_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_storage_directory(ophys_experiment_id)
+    filename = '{}_surface.tif'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def load_surface_image(ophys_experiment_id: int) -> np.ndarray:
+    image_filepath = get_surface_TIF_filepath(ophys_experiment_id)
+    image = load_image(image_filepath)
+    return image
+
+def get_depth_TIF_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_storage_directory(ophys_experiment_id)
+    filename = '{}_depth.tif'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def load_depth_image(ophys_experiment_id: int) -> np.ndarray:
+    image_filepath = get_depth_TIF_filepath(ophys_experiment_id)
+    image = load_image(image_filepath)
+    return image
+
+def get_dff_H5_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_storage_directory(ophys_experiment_id)
+    filename = '{}_dff.h5'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def get_event_H5_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_storage_directory(ophys_experiment_id)
+    filename = '{}_event.h5'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def get_local_zstack_H5_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_storage_directory(ophys_experiment_id)
+    filename = '{}_z_stack_local.h5'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def get_neuropil_traces_H5_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_processed_directory(ophys_experiment_id)
+    filename = 'neuropil_traces.h5'
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def get_roi_traces_H5_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_processed_directory(ophys_experiment_id)
+    filename = 'roi_traces.h5'
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+
+######################################################
+#             MOTION CORRECTION FILES
+######################################################
+
+def get_suite2p_rigid_motion_transform_CSV_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_processed_directory(ophys_experiment_id)
+    filename = '{}_suite2p_rigid_motion_transform.csv'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def get_suite2p_maximum_projection_PNG_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_processed_directory(ophys_experiment_id)
+    filename = '{}_suite2p_maximum_projection.png'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def load_suite2p_maximum_projection_image(ophys_experiment_id: int) -> np.ndarray:
+    image_filepath = get_suite2p_maximum_projection_PNG_filepath(ophys_experiment_id)
+    image = load_image(image_filepath)
+    return image
+
+def get_suite2p_average_projection_PNG_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_processed_directory(ophys_experiment_id)
+    filename = '{}_suite2p_average_projection.png'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def load_suite2p_average_projection_image(ophys_experiment_id: int) -> np.ndarray:
+    image_filepath = get_suite2p_average_projection_PNG_filepath(ophys_experiment_id)
+    image = load_image(image_filepath)
+    return image
+
+def get_suite2p_motion_output_H5_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_experiment_processed_directory(ophys_experiment_id)
+    filename = '{}_suite2p_motion_output.h5'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
 
 def get_suite2p_registration_summary_PNG_filepath(ophys_experiment_id: int) -> str:
     """filepath to png of image of boundary of all detected objects
@@ -241,16 +349,54 @@ def get_suite2p_registration_summary_PNG_filepath(ophys_experiment_id: int) -> s
     str
         filepath
     """
-    processed_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id)
+    storage_directory = get_experiment_processed_directory(ophys_experiment_id)
     filename = '{}__suite2p_registration_summary.png'.format(ophys_experiment_id)
-    image_filepath = os.path.join(processed_directory, filename)
-    return image_filepath
-
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
 
 def load_suite2p_registration_summary_image(ophys_experiment_id: int) -> np.ndarray:
     image_filepath = get_suite2p_registration_summary_PNG_filepath(ophys_experiment_id)
-    image = mpimg.imread(image_filepath)
+    image = load_image(image_filepath)
     return image
+
+
+
+######################################################
+#             DEMIX FILES
+######################################################
+def get_demix_traces_H5_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_demix_storage_directory(ophys_experiment_id)
+    filename = '{}_demixed_traces.h5'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def get_input_roi_demixing_JSON_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_demix_storage_directory(ophys_experiment_id)
+    filename = '{}_input_roi_demixing.json'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def get_output_roi_demixing_JSON_filepath(ophys_experiment_id: int) -> str:
+    storage_directory = get_demix_storage_directory(ophys_experiment_id)
+    filename = '{}_output_roi_demixing.json'.format(ophys_experiment_id)
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+
+
+######################################################
+#             CELL SEGMENTATION FILES
+######################################################
+def get_avgInt_TIF_filepath(ophys_experiment_id: int) -> str:
+    storage_directory  = get_current_cell_segmentation_run_directory(ophys_experiment_id)
+    filename = "avgInt.tif"
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
+
+def load_avgInt_image_image(ophys_experiment_id: int) -> np.ndarray:
+    image_filepath = get_avgInt_TIF_filepath(ophys_experiment_id)
+    image = load_image(image_filepath)
+    return image 
 
 
 def get_maxInt_boundary_PNG_filepath(ophys_experiment_id: int) -> str:
@@ -267,14 +413,15 @@ def get_maxInt_boundary_PNG_filepath(ophys_experiment_id: int) -> str:
     str
         filepath
     """
-    cell_seg_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id)
-    image_filepath = os.path.join(cell_seg_directory, "maxInt_boundary.png")
-    return image_filepath
+    storage_directory  = get_current_cell_segmentation_run_directory(ophys_experiment_id)
+    filename = "maxInt_boundary.png"
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
 
 
 def load_maxInt_boundary_image(ophys_experiment_id: int) -> np.ndarray:
     image_filepath = get_maxInt_boundary_PNG_filepath(ophys_experiment_id)
-    image = mpimg.imread(image_filepath)
+    image = load_image(image_filepath)
     return image 
 
 
@@ -293,14 +440,15 @@ def get_maxInt_masks_TIF_filepath(ophys_experiment_id: int) -> str:
     str
         filepath
     """
-    cell_seg_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id) 
-    image_filepath = os.path.join(cell_seg_directory, "maxInt_masks.TIF")
-    return image_filepath
+    storage_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id)
+    filename = "maxInt_masks.TIF"
+    filepath = get_filepath(storage_directory, filename )
+    return filepath
 
 
 def load_maxInt_masks_image(ophys_experiment_id: int) -> np.ndarray:
     image_filepath = get_maxInt_masks_TIF_filepath(ophys_experiment_id)
-    image = mpimg.imread(image_filepath)
+    image = load_image(image_filepath)
     return image 
 
 
@@ -339,14 +487,15 @@ def get_maxInt_a13a_PNG_filepath(ophys_experiment_id: int) -> str:
     str
         filepath
     """
-    cell_seg_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id) 
-    image_filepath = os.path.join(cell_seg_directory, "maxInt_a13a.png")
-    return image_filepath
+    storage_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id)
+    filename =  "maxInt_a13a.png"
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
 
 
 def load_maxInt_a13a_image(ophys_experiment_id: int) -> np.ndarray:
     image_filepath = get_maxInt_a13a_PNG_filepath(ophys_experiment_id)
-    image = mpimg.imread(image_filepath)
+    image = load_image(image_filepath)
     return image 
 
 
@@ -364,14 +513,15 @@ def get_avgInt_a1X_PNG_filepath(ophys_experiment_id: int) -> str:
     str
         filepath
     """
-    cell_seg_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id) 
-    image_filepath = os.path.join(cell_seg_directory, "avgInt_a1X.png")
-    return image_filepath
+    storage_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id)
+    filename = "avgInt_a1X.png"
+    filepath = get_filepath(storage_directory, filename )
+    return filepath
 
 
 def load_avgInt_a1X_image(ophys_experiment_id: int) -> np.ndarray:
     image_filepath = get_avgInt_a1X_PNG_filepath(ophys_experiment_id)
-    image = mpimg.imread(image_filepath)
+    image = load_image(image_filepath)
     return image 
 
 
@@ -391,14 +541,15 @@ def get_enhimgseq_TIF_filepath(ophys_experiment_id: int) -> str:
     str
         filepath
     """
-    cell_seg_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id) 
-    image_filepath = os.path.join(cell_seg_directory, "enhimgseq.TIF")
-    return image_filepath
+    storage_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id) 
+    filename = "enhimgseq.TIF"
+    filepath = get_filepath(storage_directory, filename)
+    return filepath
 
 
 def load_enhimgseq_image(ophys_experiment_id: int) -> np.ndarray:
     image_filepath = get_enhimgseq_TIF_filepath(ophys_experiment_id)
-    image = mpimg.imread(image_filepath)
+    image = load_image(image_filepath)
     return image
 
 
@@ -415,11 +566,12 @@ def get_maxInt_masks2_TIF_filepath(ophys_experiment_id: int) -> str:
     str
         filepath
     """
-    cell_seg_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id) 
-    image_filepath = os.path.join(cell_seg_directory, "maxInt_masks2.TIF")
-    return image_filepath
+    storage_directory = get_current_cell_segmentation_run_directory(ophys_experiment_id)
+    filename = "maxInt_masks2.TIF"
+    filepath = get_filepath(storage_directory, filename )
+    return filepath
 
 def load_maxInt_masks2_image(ophys_experiment_id: int) -> np.ndarray:
     image_filepath = get_maxInt_masks2_TIF_filepath(ophys_experiment_id)
-    image = mpimg.imread(image_filepath)
+    image = load_image(image_filepath)
     return image
