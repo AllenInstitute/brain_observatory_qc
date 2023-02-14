96b4fd54ffce59bd5a1e478da0f83fbdf3c00864
Matt Davis <mattjdavis@gmail.com>
2023-02-13 22:49:30 -0800

    added crop motion border

--- mindscope_qc/pipeline_dev/scripts/example/sbatch_suite2p_for_oeid.py
+++ mindscope_qc/pipeline_dev/scripts/example/sbatch_suite2p_for_oeid.py
@@ -10,9 +10,10 @@ from brain_observatory_analysis.ophys.experiment_loading import start_lamf_analy
 
 parser = argparse.ArgumentParser(description='running sbatch for new_dff_for_oeid.py')
 parser.add_argument('--env-path', type=str, default='//data/learning/mattd/miniconda3/envs/dev', metavar='path to conda environment to use')
-parser.add_argument('--dry-run', action='store_true', default=False, help='dry run')
+parser.add_argument('--dry_run', action='store_true', default=False, help='dry run')
+parser.add_argument('--test_run', action='store_true', default=False, help='test one parameter set')
 
-NONRIGID_OUTPUT_PATH = Path('/allen/programs/mindscope/workgroups/learning/pipeline_validation/nonrigid_registration_parameter_search')
+NONRIGID_OUTPUT_PATH = Path('//allen/programs/mindscope/workgroups/learning/pipeline_validation/nonrigid_registration_parameter_search')
 
 
 if __name__ == '__main__':
@@ -20,6 +21,7 @@ if __name__ == '__main__':
     python_executable = "{}/bin/python".format(args.env_path)
     print('python executable = {}'.format(python_executable))
     dry_run = args.dry_run
+    test_run = args.test_run
     base_dir = Path('//home/matt.davis/code/mindscope_qc/mindscope_qc/pipeline_dev/scripts')
     python_file = base_dir / 'suite2p_param_search.py'
 
@@ -44,18 +46,15 @@ if __name__ == '__main__':
     # # remove processed ids from ophys_experiment_ids
     # ophys_experiment_ids = [oeid for oeid in ophys_experiment_ids if oeid not in processed_ids]
 
+    ophys_experiment_ids = [1227116668]  # unstable JK manual
+    ophys_experiment_ids = [1171605650]  # jk stable
+    oeid = ophys_experiment_ids[0]
+    print(oeid)
 
-    ophys_experiment_ids = [1227116668] # unstable JK manual
-    ophys_experiment_ids = [1171605650] # jk stable
     # print number of jobs
     print('number of jobs = {}'.format(len(ophys_experiment_ids)))
 
     job_count = 0
-
-    # rerun = False
-
-    # set dry_run to True to test the script
-    dry_run = False
 
     if dry_run:
         print('dry run, exiting')
@@ -64,18 +63,35 @@ if __name__ == '__main__':
     # select 10 ids to run
     # ophys_experiment_ids = ophys_experiment_ids[:10]
 
-    for oeid in ophys_experiment_ids:
-        job_count += 1
-        print('starting cluster job for {}, job count = {}'.format(oeid, job_count))
+    bss = [128, 64, 32, 8]
+    maxnrs = [3, 6, 9, 12]
+    sts = [0, 1, 2, 5, 10]
 
-        job_title = 'oeid_{}'.format(oeid)
-        walltime = '2:00:00'
-        mem = '100G'
-        tmp = '100G',
+    # make a list of all combinations of bs, maxnr, and st
+    params = []
+    for bs in bss:
+        for maxnr in maxnrs:
+            for st in sts:
+                params.append((bs, maxnr, st))
+
+    if test_run:
+        params = params[:1]
+
+    for param in params:
+        print(param)
+        job_count += 1
+        print(f'starting cluster job for {oeid}, job count = {job_count}')
+        
+        bs, mnrs, sts = param
+        job_title = f'oeid_{oeid}_param_bs_{bs}_mnrs_{mnrs}_sts_{sts}'
+        walltime = '1:00:00'
+        mem = '55G'
+        tmp = '75G',
         job_id = Slurm.JOB_ARRAY_ID
         job_array_id = Slurm.JOB_ARRAY_MASTER_ID
         output = stdout_location / f'{job_array_id}_{job_id}_{oeid}.out'
-        cpus_per_task = 32
+        cpus_per_task = 16
+        print(output)
 
         # instantiate a SLURM object
         slurm = Slurm(
@@ -94,13 +110,14 @@ if __name__ == '__main__':
 
         # WAWRNING: this is inaccurate when interative mode on hpc
         job_id_str = '$SLURM_JOB_ID'  # get the job id from the environment variable
-        #job_id_str = str(7745718)
-        args_string = f"{oeid} {job_id_str} -t"
+        # job_id_str = str(7745718)
 
-        print('JOB_ID = {}'.format(job_id))
+        bs, mnrs, sts = param
+        args_string = f"{oeid} {job_id_str} {bs} {mnrs} {sts}"
+        print(args_string)
 
-        slurm.sbatch('{} {} {}'.format(
-                     python_executable,
-                     python_file,
-                     args_string,))
+        sbatch_string = f"{python_executable} {python_file} {args_string}"
+        print(sbatch_string)
+        slurm.sbatch(sbatch_string)
         time.sleep(0.01)
+# 1171605650 $SLURM_JOB_ID -t
--- mindscope_qc/pipeline_dev/scripts/suite2p_param_search.py
+++ mindscope_qc/pipeline_dev/scripts/suite2p_param_search.py
@@ -3,39 +3,59 @@ from suite2p import run_s2p, default_ops
 from pathlib import Path
 import os
 import argparse
+import h5py
+
+from depth_estimation_module import get_motion_correction_crop_xy_range
 
 # set your options for running
 NONRIGID_OUTPUT_PATH = Path('/allen/programs/mindscope/workgroups/learning/pipeline_validation/nonrigid_registration_parameter_search')
 SCRATCH_PATH = Path('')
 
-# argparse ophys_experiment_id
+# argparse oeid
 parser = argparse.ArgumentParser()
-parser.add_argument("ophys_experiment_id", type=int, help="ophys_experiment_id")
+parser.add_argument("oeid", type=int, help="oeid")
 parser.add_argument("job_id", type=int, help="path to scratch space")
-parser.add_argument("--test_run", "-t", action="store_true", help="test_run")
+parser.add_argument("block_size", type=int, help="block size")
+parser.add_argument("max_nr_shift", type=int, help="max_nr_shift")
+parser.add_argument("smooth_time_sigma", type=int, help="smooth_time_sigma")
+parser.add_argument("--single_run", "-s", action="store_true", help="single_run")
 parser.add_argument("--grid_seach", "-g", action="store_true", help="grid seach")
 
 
-def run_s2p_with_params(ophys_experiment_id, scratch_path, params):
+def run_s2p_with_params(oeid, scratch_path, params):
     """Run suite2p on a set of input parameters
  
     Note: Need to be on mjd_dev of Suite2p bracat nch, which contains h5 file handling
     """
     ops = default_ops()
 
-    scratch_path = Path("/scratch/fast") / str(scratch_path) 
+    scratch_path = Path("/scratch/fast") / str(scratch_path)
 
     print(f"SCRATCH PATH = {scratch_path}")
     # create string out of params key/value pairs
     param_string = ""
     for key, value in params.items():
-        param_string += f"{key}={value}_"
-    save_folder = f"{ophys_experiment_id}_{param_string[:-1]}"
+        param_string += f"{key}{value}_"
+    save_folder = f"{oeid}_{param_string[:-1]}"
 
     fast_disk_path = scratch_path / save_folder
 
     # get the h5 file
-    h5_file = str(from_lims.get_motion_corrected_movie_filepath(ophys_experiment_id))
+    h5_file = str(from_lims.get_motion_corrected_movie_filepath(oeid))
+    print(f"Running suite2p on {h5_file} with params {params}")
+
+    # load the h5 file and crop, save to srcatch space
+    
+    with h5py.File(h5_file, 'r') as f:
+        frames = f['data']
+    y_range, x_range = get_motion_correction_crop_xy_range(oeid)
+    print(f"CROP RIGID: Saving cropped h5 file to scratch space, borders: y={y_range}, x={x_range}")
+    cropped_h5_file = str(scratch_path / f"{oeid}_cropped.h5")
+    with h5py.File(cropped_h5_file, 'w') as f:
+        f.create_dataset('data', data=frames[:, y_range[0]:y_range[1], x_range[0]:x_range[1]])
+    h5_file = cropped_h5_file
+
+    #h5_file = '/allen/programs/mindscope/workgroups/learning/pipeline_validation/nonrigid_registration_parameter_search/1000_frame_test.h5'
 
     # ana = "ana" if params["anatomical_only"] else ''
 
@@ -64,13 +84,13 @@ def run_s2p_with_params(ophys_experiment_id, scratch_path, params):
         'delete_bin': False,
 
         # data settings
-        'fs': 11,
+        'fs': 10.7,
         'tau': params['tau'],
         # 'frames_include':250
 
         # reg
         'nonrigid': True,
-        'block_size': params['bs'],
+        'block_size': [params['bs'], params['bs']],
         'smooth_time_sigma': params['sts'],
         'maxregshiftNR': params['mnrs'],
 
@@ -98,7 +118,9 @@ def run_s2p_with_params(ophys_experiment_id, scratch_path, params):
         # extra setting added by MJD
         "h5_file_list": None,
         "fast_bin_path": fast_bin_path,  # None, #ast_path,
-        'extraction': True
+        'extraction': True,
+        'save_segmented_tiff': True,
+        'reg_tif': True,  # suite2p setting must be true for segmented
     }
 
     opsEnd = run_s2p(ops=ops, db=db)
@@ -109,23 +131,24 @@ def run_s2p_with_params(ophys_experiment_id, scratch_path, params):
 if __name__ == "__main__":
 
     args = parser.parse_args()
-    expt_id = args.ophys_experiment_id
+    expt_id = args.oeid
     job_id = args.job_id
-    test = args.test_run
+    block_size = args.block_size
+    max_nr_shift = args.max_nr_shift
+    smooth_time_sigma = args.smooth_time_sigma
+    single_run = args.single_run
     grid_search = args.grid_seach
 
-    assert not (test and grid_search), "Cannot specify both test and grid_search"
-    assert expt_id is not None, "Must specify ophys_experiment_id"
+    assert not (single_run and grid_search), "Cannot specify both single_run and grid_search"
+    assert expt_id is not None, "Must specify oeid"
 
-    bss = [[8, 8], [32, 32], [64, 64], [96, 96], [128, 128]]
-    maxnrs = [3, 6, 9, 12]
-    sts = [0, 1, 2, 5, 10]
+    # block_size = [block_size, block_size]
+    single_run = True
+    if single_run:
 
-    if test:
-        print("TEST RUN")
-        params = {"sts": 0,
-                  "bs": [64, 64],
-                  "mnrs": 5,
+        params = {"sts": smooth_time_sigma,
+                  "bs": block_size,
+                  "mnrs": max_nr_shift,
                   "ts": 2.0,
                   "dia": 15,
                   "tau": 0.7,
@@ -134,6 +157,9 @@ if __name__ == "__main__":
         finals_ops = run_s2p_with_params(expt_id, job_id, params)
 
     if grid_search:
+        bss = [[8, 8], [32, 32], [64, 64], [96, 96], [128, 128]]
+        maxnrs = [3, 6, 9, 12]
+        sts = [0, 1, 2, 5, 10]
         for bs in bss:
             for maxnr in maxnrs:
                 for st in sts:
