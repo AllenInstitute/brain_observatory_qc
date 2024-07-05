5815d41678b65b6f80d54787629635594f128b24
Clark Roll <kater@alleninstitute.org>
2024-07-05 12:08:47 -0700

    updated plotting functions
    generic function for plotting matrices. Specific functions for plotting impacted data outcome and qc review status

--- brain_observatory_qc/visualizations/project_reports/plotting_functions.py
+++ brain_observatory_qc/visualizations/project_reports/plotting_functions.py
@@ -3,46 +3,42 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import brain_observatory_qc.data_access.from_mouseQC as mqc
-
-pass_flag_fail_color_mapping = {
-    'pass': '#4CAF50',  # Green
-    'flag': '#FFC107',  # Amber
-    'fail': '#F44336'   # Red
+pass_flag_fail_palette = {
+    'pass'   : '#4CAF50', # Green
+    'flag'   : '#FFC107', # Amber
+    'fail'   : '#F44336', # Red
+    'missing': '#D3D3D3', # Light gray
 }
 
-qc_submit_status_palette = {
-"ready":"#dfc27d",    # ready
-"error":"#a6611a",     # error
-"complete":"#018571",  # complete
-"incomplete":"#80cdc1" # incomplete
+qc_gen_sub_status_palette = {
+     "error"     :"#a6611a",  # brown
+     "complete"  :"#940094",  # purple
+     "incomplete":"#4a0094",  # blue
+     "pass"      :"#8dd3c7",  # slightly desaturated cyan
+     "flag"      :"#ffffb3",  # light yellow
+     "fail"      :"#fb8072"   # light red
 }
 
-data_stream_status_palette = {
-"not_reported":"#bebada",  # Not Reported
-"pass":"#8dd3c7",          # pass
-"flag":"#ffffb3",          # flag
-"fail":"#fb8072"           # fail
-}
 
-def plot_status_by_id_matrix(df:pd.DataFrame,
-                            id_col:str='data_id',
-                            color_mapping:dict = data_stream_status_palette,
-                            save_path:str=None,
-                            xlabel:str=None,
-                            ylabel:str=None,
-                            title:str=None,
-                            figsize:tuple=(10,8)):
-    """a generic function that will take a dataframe of a specific struture
+def plot_status_by_id_matrix(df: pd.DataFrame,
+                             id_col: str,
+                             color_mapping: dict,
+                             save_path: str = None,
+                             xlabel: str = None,
+                             ylabel: str = None,
+                             title: str = None,
+                             figsize: tuple = (10, 8),
+                             show_labels: bool = True):
+    """a generic function that will take a dataframe of a specific structure
     and create a colored grid plot based on the values in the dataframe
     y axis: ids 
-    x axis: all other colums in the dataframe
+    x axis: all other columns in the dataframe
 
     Parameters
     ----------
     df : pd.DataFrame
         a dataframe with a specific structure:
-        - an id column (e.g. ophys_session_id, ophys_experiment_id)
+        - first column is an id column (e.g. ophys_session_id, ophys_experiment_id)
         - all remaining columns contain string values that are keys in the color_mapping dictionary
         the strings represent the status of the given column
 
@@ -52,19 +48,27 @@ def plot_status_by_id_matrix(df:pd.DataFrame,
     color_mapping : dict
         the color mapping dictionary that maps the string values in the dataframe to colors
     figsize : tuple, optional
-        _description_, by default (10,8)
+        size of the figure, by default (10,8)
     save_path : str, optional
-        _description_, by default None
+        location to save the plot, by default None
+    show_labels : bool, optional
+        whether to show labels on the patches, by default True
     """
+    # Define default color for NaN values
+    default_color = color_mapping.get('missing', '#D3D3D3')  # Light gray as default
+
     # Create a color matrix based on the DataFrame values
-    color_matrix = df.iloc[:, 1:].applymap(lambda x: color_mapping[x])
+    color_matrix = df.iloc[:, 1:].applymap(lambda x: color_mapping.get(x, default_color))
 
     # Plotting the colored grid
     fig, ax = plt.subplots(figsize=figsize)
 
     # Create a grid of colored boxes
     for (row_idx, col_idx), val in np.ndenumerate(df.iloc[:, 1:].values):
-        ax.add_patch(plt.Rectangle((col_idx, row_idx), 1, 1, color=color_mapping[val]))
+        color = color_mapping.get(val, default_color)
+        ax.add_patch(plt.Rectangle((col_idx, row_idx), 1, 1, color=color))
+        if show_labels:
+            ax.text(col_idx + 0.5, row_idx + 0.5, str(val), ha='center', va='center', color='black')
 
     # Set ticks and labels
     ax.set_xticks(np.arange(len(df.columns[1:])) + 0.5)
@@ -92,6 +96,11 @@ def plot_status_by_id_matrix(df:pd.DataFrame,
     if title:
         ax.set_title(title)
 
+    # Create a custom legend
+    handles = [plt.Rectangle((0, 0), 1, 1, color=color_mapping[key]) for key in color_mapping.keys()]
+    labels = list(color_mapping.keys())
+    ax.legend(handles, labels, title="Legend", bbox_to_anchor=(1.05, 1), loc='upper left')
+
     # Save the plot if a save path is provided, otherwise show the plot
     if save_path:
         plt.savefig(save_path, bbox_inches='tight')
@@ -99,12 +108,61 @@ def plot_status_by_id_matrix(df:pd.DataFrame,
         plt.show()
     
 
-    def plot_data_stream_outcomes_matrix(data_stream_outcomes_df, 
-                                         id_col = "data_id",
-                                         save_path=None):
+def plot_impacted_data_outcomes_matrix(data_stream_outcomes_df: pd.DataFrame, 
+                                       id_col: str = "data_id",
+                                       save_path: str = None,
+                                       show_labels: bool = True):
+    """ Plot a matrix of the QC outcomes for each impacted data stream or context.
+    Input dataframe should be limited to sessions/exeriments that have COMPLETED
+    QC Generation and Review.
+
+    Parameters
+    ----------
+    data_stream_outcomes_df : pd.DataFrame
+        table with a column for data_id and columns for each impacted
+        data (data streams, data context) with values of "pass", "flag", "fail"
+    id_col : str, optional
+        name of column that has the data id (i.e. ophys session id, etc),
+        by default "data_id"
+    save_path : str, optional
+        location to save the plot, by default None
+    show_labels : bool, optional
+        whether to show labels on the patches, by default True
+    """
+    plot_status_by_id_matrix(data_stream_outcomes_df,
+                             id_col=id_col,
+                             color_mapping=pass_flag_fail_palette,
+                             save_path=save_path,
+                             xlabel="Impacted Data",
+                             ylabel="Data ID",
+                             title="Impacted Data QC Outcomes",
+                             show_labels=show_labels)
 
 
-    def plot_qc_status_by_id_matrix(mongo_connection, 
-                                start_date, 
-                                end_date, 
-                                save_path=None):
+def plot_qc_submit_status_matrix(submit_status_df:pd.DataFrame, 
+                                 id_col:str = "data_id",
+                                 save_path:str=None,
+                                 show_labels: bool = True):
+    """ Plot a matrix of the QC generation and submission status for each
+     dataset (session, experiment etc.)
+
+        Parameters
+        ----------
+        submit_status_df : pd.DataFrame
+            table with the following columns:
+                - generation_status
+                - review_status
+                - qc_outcome
+        id_col : str, optional
+           name of column for data unit, examples: "ophys_session_id", by default "data_id"
+        save_path : str, optional
+            location to save the plot, by default None
+    """
+    plot_status_by_id_matrix(submit_status_df,
+                            id_col=id_col,
+                            color_mapping=qc_gen_sub_status_palette,
+                            save_path=save_path,
+                            xlabel="QC Status",
+                            ylabel="Data ID",
+                            title="QC Generation & Submission Status",
+                            show_labels=show_labels)
